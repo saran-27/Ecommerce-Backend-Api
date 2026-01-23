3d9731cc2cf1e0330a4715796895df89d34d1495
Saravanan_27 <jacksaran243@gmail.com>
2026-01-23 09:59:04 +0530

    database

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -81,13 +81,11 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-import dj_database_url
-import os
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
--- ecommerce/ecommerce_app/Serializer.py
+++ ecommerce/ecommerce_app/Serializer.py
@@ -1,5 +1,4 @@
 from rest_framework.serializers import ModelSerializer
-from yaml import serialize
 from rest_framework import serializers
 
 from .models import *
