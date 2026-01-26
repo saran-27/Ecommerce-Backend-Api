ca359709282351f3ec7fed3deef2bce2b1fb89ca
Saravanan_27 <jacksaran243@gmail.com>
2026-01-26 14:24:44 +0530

    image key

--- ecommerce/ecommerce_app/models.py
+++ ecommerce/ecommerce_app/models.py
@@ -13,6 +13,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='products')
+    image_key = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
