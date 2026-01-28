3f141444b2dcdb79bfe1bce04f7a5e89e502706c
Saravanan_27 <jacksaran243@gmail.com>
2026-01-28 08:19:35 +0530

    img

--- ecommerce/ecommerce_app/models.py
+++ ecommerce/ecommerce_app/models.py
@@ -13,9 +13,17 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='products')
-    image_key = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    image_key = models.CharField(
+        max_length=100,
+        help_text="Use lowercase keys like play_station, tshirt_1"
+    )
+
+    def save(self, *args, **kwargs):
+        if self.image_key:
+            self.image_key = self.image_key.lower().strip()
+        super().save(*args, **kwargs)
     def __str__(self):
         return self.name
 
